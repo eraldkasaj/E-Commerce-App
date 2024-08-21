8364396607101c5fcf245bc9ed68adffd81b6e5e
Aldi Kasaj <eraldkasaj14@gmail.com>
2024-08-21 21:48:01 +0200

    add related products

--- e-commerce-app/src/Pages/Product.jsx
+++ e-commerce-app/src/Pages/Product.jsx
@@ -3,6 +3,7 @@ import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom'
 import Breadcrum from '../Components/Breadcrums/Breadcrum'
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts'
 
 const Product = () => {
     const {all_product} = useContext(ShopContext)
@@ -12,6 +13,7 @@ const Product = () => {
         <div>
             <Breadcrum product={product}/>
             <ProductDisplay product={product}/>
+            <RelatedProducts/>
         </div>
     )
 }
